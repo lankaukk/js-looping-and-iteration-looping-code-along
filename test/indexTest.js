80fd08f513c8e0cf0bee0edc05d56a25665bd62a
McKayla Lankau <lankaukk@yahoo.com>
2021-08-09 23:05:56 -0400

    commit tree 8c10345f73da3bc3470b95c252bde042e80314ec

--- test/indexTest.js
+++ test/indexTest.js
@@ -14,8 +14,8 @@ describe( 'index.js', () => {
   describe( 'writeCards()', () => {
 
     it( 'returns an array of thank you messages for each name provided to the function', () => {
-      expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
-        .to.deep.eq( [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ] );
+      expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'birthday' ) )
+        .to.deep.eq( [ 'Thank you, Lisa, for the wonderful birthday gift!', 'Thank you, Kaitlin, for the wonderful birthday gift!', 'Thank you, Jan, for the wonderful birthday gift!' ] );
     } );
   } );
 

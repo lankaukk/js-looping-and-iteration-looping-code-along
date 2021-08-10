4739f9ab96be381f556bc470e58bbb02e18fc72b
McKayla Lankau <lankaukk@yahoo.com>
2021-08-09 22:51:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// Code your solutions in this file
+
+function writeCards(name, event){
+    let namesArray = [];
+    for (let i = 0; i < name.length; i++){
+        namesArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return namesArray;
+}
+
+function countDown(int){
+    let count = int;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
